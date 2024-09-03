0f3fd192425b94fdce6364009cdb8903866138fc
WahidMuhammadDjati <wahiddjati5@gmail.com>
2024-09-03 14:17:37 +0700

    registrasi

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home', ['title' => 'Home']);
 });
 Route::get('/admin', [MarketController::class, 'index']);
