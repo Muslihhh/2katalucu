34ce1ba74ecb7d845592219f2a2c76cc044fc306
unknown <rifaimuh760@gmail.com>
2024-09-13 09:29:51 +0700

    a

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -472,10 +472,6 @@
         </div>
         <div class="space-y-4 sm:space-y-6">
             <div>
-                <label for="product-brand" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Penjual</label>
-                <input type="text" id="product-brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="Product Brand" required="">
-            </div>
-            <div>
                 <label for="product-price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
                 <input type="number" id="product-price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="" placeholder="Product price" required="">
             </div>
--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -1,29 +1,22 @@
 <x-layout>
-<<<<<<< HEAD
-    <x-slot:title>{{ $title }}</x-slot:title>
-    
-    <div class="bg-white w-full border shadow-2xl rounded-md p-5 flex gap-5">
-        <div class="w-1/2">
-=======
-<<<<<<< HEAD
     <x-slot:title>{{ $title }}</x-slot:title>
       <div class="bg-white lg:flex sm:content-center w-full border rounded-md p-5 gap-5">
         <div class="lg:w-1/2 sm:w-full">
-          <img class="w-full rounded-md" src="https://i.pinimg.com/736x/28/7c/b5/287cb5bee3282583abe7836cc2f15465.jpg" alt="">
+          <img class="w-full rounded-md" src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" alt="">
         </div>
         <div class="lg:w-1/2 sm:w-full">
             <div class="h-1/2">
                 <h1 class=" text-3xl font-bold border-b border-black ">Nama Produk</h1>
-                <h2 class=" text-2xl py-5 ">$99</h2>
+                <h2 class=" text-2xl py-5 ">${{ $product->price }}</h2>
                 <i class="fa fa-star-o"></i>
                 <i class="fa fa-star-o"></i>
                 <i class="fa fa-star-o"></i>
                 <i class="fa fa-star-o"></i>
                 <i class="fa fa-star-o"></i>
                 <h3 class=" underline-offset-4 underline">Detail</h3>
-                <p>Stok : </p>
-                <p>Kategori : </p> 
-                <p>Deskripsi : </p>
+                <p>Stok : {{ $product->stock ?? 'N/A' }}</p>
+                <p>Kategori : {{ $product->category->name ?? 'N/A' }}</p> 
+                <p>Deskripsi : {{ $product->description }}</p>
             </div>
             <div class="flex gap-5 h-1/2 items-end ">
               <a href="http://" class="w-1/2">
@@ -51,43 +44,3 @@
       </div>
       </div>
 </x-layout>
-=======
-  <x-slot:title>{{ $title }}</x-slot:title>
-  <div class="bg-white w-full border shadow-2xl rounded-md p-5 flex gap-5">
-      <div class="w-1/2">
-        @foreach ($products as $product)
-        <a href="{{ route('products.show', $product->id) }}">
->>>>>>> 6becfc5b706f3367a2d7913259fc92eb42437bed
-            <img src="{{ asset('storage/images/' . $product->image) }}" alt="{{ $product->name }}">
-        </div>
-        <div class="w-1/2">
-            <div class="h-1/2">
-                <h1 class="text-3xl font-bold border-b border-black">{{ $product->name }}</h1>
-                <h2 class="text-2xl py-5">${{ $product->price }}</h2>
-                <i class="fa fa-star-o"></i>
-                <i class="fa fa-star-o"></i>
-                <i class="fa fa-star-o"></i>
-                <i class="fa fa-star-o"></i>
-                <i class="fa fa-star-o"></i>
-                <h3 class="underline-offset-4 underline">Detail</h3>
-                <p>Deskripsi: {{ $product->description }}</p>
-                <p>Stok: {{ $product->stock ?? 'N/A' }}</p>
-                <p>Kategori: {{ $product->category->name ?? 'N/A' }}</p>
-            </div>
-            <div class="flex gap-5 h-1/2 items-end">
-                <button class="fa fa-shopping-cart w-1/2 h-14 transition duration-150 ease-in-out hover:scale-110 bg-green-500 hover:bg-green-600 text-white font-bold py-3 rounded-md">Add to Cart</button>
-                <a href="{{ route('sendWhatsAppLink') }}" class="btn btn-primary">
-                    <button class="fa fa-whatsapp h-14 transition duration-150 ease-in-out hover:scale-110 bg-green-500 hover:bg-green-600 text-white font-bold py-3 rounded-md w-auto px-20">Order</button>
-                </a>
-                <a href="{{ route('products.create') }}">
-                    <button class="fa fa-whatsapp h-14 transition duration-150 ease-in-out hover:scale-110 bg-green-500 hover:bg-green-600 text-white font-bold py-3 rounded-md w-auto px-20">Tambah</button>
-                </a>
-            </div>
-        </div>
-    </div>
-</x-layout>
-<<<<<<< HEAD
-    
-=======
->>>>>>> a4da50c6a26fb45cab4c12bbf5f5f12934876dd2
->>>>>>> 6becfc5b706f3367a2d7913259fc92eb42437bed
