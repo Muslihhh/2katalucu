e1532cfd326511b13d337beef7b81de992505e01
ianbrilian <Nandaiyan55@gmail.com>
2024-10-08 14:45:10 +0700

    checkout

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -588,6 +588,7 @@
                     </div>
                 </div>
             </div>
+            
             <div class="overflow-x-auto">
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
