971c744b7b321a1cd0a9926a301b29530e13b078
unknown <rifaimuh760@gmail.com>
2024-10-15 12:01:21 +0700

    a

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -31,7 +31,7 @@
   {{-- :categoriesRoute="route('home.filter')" --}}
   <x-navbar :homeRoute="route('home')" :categories="$categories" :products="$products" />
 <main>
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <div class="mx-auto max-w-full px-4 py-6 sm:px-6 lg:px-8">
         {{ $slot }}
       </div>
     </main>
