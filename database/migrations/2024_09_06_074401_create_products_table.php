c241333fc3d5e055eed49414874c5c3deb5454ae
unknown <rifaimuh760@gmail.com>
2024-10-16 16:27:09 +0700

    a

--- database/migrations/2024_09_06_074401_create_products_table.php
+++ database/migrations/2024_09_06_074401_create_products_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
              $table->foreign('category_id')
              ->references('id')->on('categories')
              ->onDelete('cascade');
-             $table->string('image')->nullable();  // Tambahkan kolom image
+              // Tambahkan kolom image
              $table->timestamps();
          });
      }
