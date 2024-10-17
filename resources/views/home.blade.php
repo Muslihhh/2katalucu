c8ee6b9c04f60e6ef0372a2cb23590377d500b6d
unknown <rifaimuh760@gmail.com>
2024-10-17 15:04:10 +0700

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -108,7 +108,7 @@
                                             New </span>
                                     @endif
                                     <h3 class="mt-4 text-lg font-medium text-gray-900">{{ $product->name }}</h3>
-                                    <p class="mt-1.5 text-sm text-gray-700">${{ number_format($product->price, 2) }}
+                                    <p class="mt-1.5 text-sm text-gray-700">Rp.{{ number_format($product->price, 2) }}
                                     </p>
                                     <form class="mt-4">
                                         <button
